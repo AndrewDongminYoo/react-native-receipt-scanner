1f6a92bfc85090e739eebe0132a6673b7b82943e
Dongmin, Yu <ydm2790@gmail.com>
2026-06-30 18:59:16 +0900

    chore: ignore local OCR test fixtures

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: "@react-native/jest-preset",
   modulePathIgnorePatterns: ["<rootDir>/example/node_modules", "<rootDir>/lib/"],
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/src/__tests__/fixtures/"],
 };
